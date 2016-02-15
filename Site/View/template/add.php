6be71288b56e850ad9578030f24dd82ccd638c66
hannan <hannan@skyimpel.com>
2016-02-15 14:42:29 +0500

    Template view changed for upload

--- Site/View/template/add.php
+++ Site/View/template/add.php
@@ -8,18 +8,24 @@ $token = Helper\Protection::viewPrivateToken();
 
 ?>
 
-<?php Helper\Page::setTitle('Add Template'); ?>
-<?php Helper\Page::setIndex('template'); ?>
+<?php Helper\Page::setTitle('Templates'); ?>
+<?php Helper\Page::setIndex('templates'); ?>
 <?php Helper\Page::includes('header'); ?>
 <?php Helper\Page::includes('top'); ?>
 
 <div class="panel user-panel">
     <div class="panel-heading">
-        <h1>Add Template</h1>
+        <h1>Template</h1>
     </div>
     
     <div class="panel-body">
         <div class="col-sm-4">
+    <div class="alert alert-info col-sm-12" role="alert">
+        <br>
+        <p><strong>Comming soon!</strong> Section is under construction.</p>
+        <br>
+    </div>
+           
             <form class="validate" action="<?=Helper\Link::route('user/account/login')?>" method="post">
                 <?=Helper\Protection::viewPublicTokenField()?>
                 
